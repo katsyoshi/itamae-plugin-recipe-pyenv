1eb2345e4792bf28844dc16a61ef6e37c59bf40b
yusaku.hatanaka <hata.yusaku.1225@gmail.com>
2017-04-29 17:27:23 +0900

    move install logic to install.rb

--- lib/itamae/plugin/recipe/pyenv/system.rb
+++ lib/itamae/plugin/recipe/pyenv/system.rb
@@ -1,29 +1,9 @@
-case node[:platform]
-when "debian", "ubuntu"
-  package "build-essential"
-end
+node.reverse_merge!(
+  pyenv: {
+    pyenv_root: '/usr/local/pyenv',
+    scheme:     'git',
+    versions:   [],
+  },
+)
 
-package "git"
-
-require "itamae/plugin/recipe/pyenv"
-
-scheme = "git"
-scheme = node[:pyenv][:scheme] if node[:pyenv][:scheme]
-
-git pyenv_root do
-  repository "#{scheme}://github.com/yyuu/pyenv.git"
-end
-
-node[:pyenv][:versions].each do |version|
-  execute "pyenv install #{version}" do
-    command "#{pyenv_init} pyenv install #{version}"
-    not_if  "#{pyenv_init} pyenv versions | grep #{version}"
-  end
-end
-
-node[:pyenv][:global].tap do |version|
-  execute "pyenv global #{version}" do
-    command "#{pyenv_init} pyenv global #{version}"
-    not_if  "#{pyenv_init} pyenv version | grep #{version}"
-  end
-end
+include_recipe 'pyenv::install'
