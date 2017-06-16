9eb191994e6ff30cb9201d261aaf975052182a50
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2017-06-16 20:56:19 +0900

    node.pyenv as pyenv

--- lib/itamae/plugin/recipe/pyenv/install.rb
+++ lib/itamae/plugin/recipe/pyenv/install.rb
@@ -9,30 +9,31 @@ end
 
 package 'git'
 
-git node.pyenv.pyenv_root do
-  repository "#{node.pyenv.scheme}://github.com/pyenv/pyenv.git"
-  user node.pyenv.user if node.pyenv.user
+pyenv = node.pyenv
+
+git pyenv.pyenv_root do
+  repository "#{pyenv.scheme}://github.com/pyenv/pyenv.git"
+  user node.pyenv.user if pyenv.user
 end
 
-pyenv_init = <<-EOS
-  export PYENV_ROOT=#{node.pyenv.pyenv_root}
-  export PATH="#{node.pyenv.pyenv_root}/bin:$PATH"
+pyenv_init = <<~EOS
+  export PYENV_ROOT=#{pyenv.pyenv_root}
+  export PATH="#{pyenv.pyenv_root}/bin:$PATH"
   eval "$(pyenv init -)"
 EOS
 
-node.pyenv.versions.each do |version|
+pyenv.versions.each do |version|
   execute "pyenv install #{version}" do
     command "#{pyenv_init} pyenv install #{version}"
     not_if  "#{pyenv_init} pyenv versions | grep #{version}"
-    user node.pyenv.user if node.pyenv.user
+    user pyenv.user if pyenv.user
   end
 end
 
-node.pyenv.global.tap do |version|
+pyenv.global.tap do |version|
   execute "pyenv global #{version}" do
     command "#{pyenv_init} pyenv global #{version}"
     not_if  "#{pyenv_init} pyenv version | grep #{version}"
-    user node.pyenv.user if node.pyenv.user
+    user pyenv.user if pyenv.user
   end
 end
-
