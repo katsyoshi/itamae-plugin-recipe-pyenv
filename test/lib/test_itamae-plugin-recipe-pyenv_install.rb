9f15ce87b6caa7b5b427e7b34da43b79de6e3dd4
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-07-10 09:14:10 +0900

    add test case

--- test/lib/test_itamae-plugin-recipe-pyenv_install.rb
+++ test/lib/test_itamae-plugin-recipe-pyenv_install.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class ItamaePluginRecipePyenvTest < Test::Unit::TestCase
-  def test_installed
+  def test_system_installed
     assert File.exist?("/usr/local/pyenv/"), "cannot find pyenv in system"
-    assert File.exist?("/home/itamae/pyenv/"), "cannot find pyenv in user"
+  end
+
+  def test_system_configuration
+    assert File.exist?("/etc/profile.d/90.pyenv.sh"), "cannot find config in profile.d"
+  end
+
+  def test_user_installed
+    assert File.exist?("/home/itamae/.pyenv/"), "cannot find pyenv in user"
   end
 end
