27d8a63c167d64f78a805914c23e43784f8c87b8
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-07-11 21:20:00 +0900

    remove unnecessary test

--- test/lib/test_itamae-plugin-recipe-pyenv_install.rb
+++ test/lib/test_itamae-plugin-recipe-pyenv_install.rb
@@ -5,10 +5,6 @@ class ItamaePluginRecipePyenvTest < Test::Unit::TestCase
     assert File.exist?("/usr/local/pyenv/"), "cannot find pyenv in system"
   end
 
-  def test_system_configuration
-    assert File.exist?("/etc/profile.d/90.pyenv.sh"), "cannot find config in profile.d"
-  end
-
   def test_user_installed
     assert File.exist?("/home/itamae/.pyenv/"), "cannot find pyenv in user"
   end
